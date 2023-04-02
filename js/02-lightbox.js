ce229a1646f037e368cf5a202cab070df532f4df
Yuliia Pacheva <juliwoood@gmail.com>
2023-04-03 00:21:01 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -19,30 +19,11 @@ function createGalleryMarkup(images) {
 // Додаю розмітку в DOM
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
+// Все інше робить SimpleLightbox
 galleryConteiner.insertAdjacentHTML("afterbegin", galleryMarkup);
 
-// Додаю слухача, який буде слухати клік
-galleryConteiner.addEventListener('click', onImgClick);
-
-
-function onImgClick(evt) {
-  // Прибираю нативну поведінку під час кліку по зображенню
-  evt.preventDefault();
-
-  // Відбираю, щоб ф-ція спрацьовувала лише по кліку на зображення, а за його межами - ні
-//   const isImgEl = evt.target.classList.contains('gallery__image');
-//   if (!isImgEl) {
-//     return;
-//   }
-    console.log(evt.target);
-    
     new SimpleLightbox('.gallery a', { 
-        captions: true,
-        captionSelector: 'img',
-        captionType: 'attr',
         captionsData: 'alt',
-        captionPosition: 'bottom',
         captionDelay: 250,
-        spinner: false,
     });  
-}
+
